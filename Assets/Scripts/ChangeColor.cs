85378a0853a39823cb5ed376900436b18bb5f98c
Adam Zmudzinski <adamzmudzinski2@gmail.com>
2020-02-01 13:01:06 -0500

    Add game color state

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite[] sprites = null;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        GameColor.onChangeColor += Change;
+    }
+
+    private void OnDestroy() {
+        GameColor.onChangeColor -= Change;
+    }
+
+    private void Change(int newColorState) {
+        spriteRenderer.sprite = sprites[newColorState];
+    }
+
+}
