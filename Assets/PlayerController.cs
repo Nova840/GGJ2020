edd6a791269e957eb816ae3e2604ce0182c26fc7
Adam Zmudzinski <adamzmudzinski2@gmail.com>
2020-02-01 13:48:09 -0500

    Fix player jumping bug

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     [SerializeField]
     private int playerNum;
     [SerializeField]
@@ -15,92 +14,42 @@ public class PlayerController : MonoBehaviour
     private float jumpScale;
 
     private Rigidbody2D rb2;
-    //private bool isOnGround = false;
-    //TODO Fix this later
+
     [SerializeField]
     private LayerMask groundLayer;
 
-
-    private void Start()
-    {
+    private void Start() {
         rb2 = this.gameObject.GetComponent<Rigidbody2D>();
     }
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        float x_movement = Input.GetAxis("Horizontal"+playerNum);
-        
-        if (rb2.velocity.magnitude < maxSpeed)
-        {
+
+    void FixedUpdate() {
+        float x_movement = Input.GetAxis("Horizontal" + playerNum);
+
+        if (rb2.velocity.magnitude < maxSpeed) {
             Vector2 movement = new Vector2(x_movement, 0);
             rb2.AddForce(movement * scale);
         }
-        if (IsGrounded()) {
-            if (Input.GetButtonDown("Jump" + playerNum))
-        {
-         
-                //isOnGround = false;
+
+        if (Input.GetButtonDown("Jump" + playerNum)) {
+            if (IsGrounded()) {
                 Vector2 jumpForce = new Vector2(0, jumpScale * 10);
                 rb2.AddForce(jumpForce);
             }
         }
 
     }
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (CollisionIsWithGround(collision))
-    //    {
-    //        isOnGround = true;
-    //        print(isOnGround);
-    //    }
 
-    //}
-    //private void OnCollisionExit2D(Collision2D collision)
-    //{
-    //    if(!CollisionIsWithGround(collision))
-    //    {
-    //        isOnGround = false;
-    //        print(isOnGround);
-    //    }
-    //}
 
-    private bool CollisionIsWithGround(Collision2D collision)
-    {
-        bool is_with_ground = false;
-        foreach (ContactPoint2D c in collision.contacts)
-        {
-            Vector2 colDirVector = c.point + rb2.position;
-                                     if(colDirVector .y <0)
-            {
-                is_with_ground = true;
-            }
+    public bool IsGrounded() {
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = .5f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        Debug.DrawRay(position, direction * (hit ? hit.distance : distance), hit ? Color.green : Color.red);
+        if (hit.collider != null) {
+            return true;
         }
-
-        return is_with_ground;
-    }
-
-
-    public bool IsGrounded()
-    {
-        //Todo Fix Raycasting solution
-                        Vector2 position = transform.position;
-                Vector2 direction = Vector2.down;
-                float distance = .5f;
-
-                Debug.DrawRay(position, direction * distance, Color.green);
-                RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
-                print(hit.collider != null);
-                if (hit.collider != null)
-                {
-                    return true;
-            
-                }
-
-                return false;
-
-
-
-
-        //return true;
+        return false;
     }
 }
