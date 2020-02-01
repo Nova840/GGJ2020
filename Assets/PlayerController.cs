f8405e75793d1fe0c14a622ac4b8a25bd00c46b0
Victor  Payano <vick.sensei@gmail.com>
2020-02-01 12:45:41 -0500

    Added Jumping
    Jumping working with collision detection. avoid platforms smaller than colliders

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,8 +11,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float maxSpeed;
 
+    [SerializeField]
+    private float jumpScale;
 
     private Rigidbody2D rb2;
+    private bool isOnGround = false;
+    //TODO Fix this later
+//    [SerializeField]
+//    private LayerMask groundLayer;
+
 
     private void Start()
     {
@@ -21,13 +28,77 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float x_movement = Input.GetAxis("Horizontal");
-
+        float x_movement = Input.GetAxis("Horizontal"+playerNum);
+        
         if (rb2.velocity.magnitude < maxSpeed)
         {
             Vector2 movement = new Vector2(x_movement, 0);
             rb2.AddForce(movement * scale);
         }
+        if (Input.GetButtonDown("Jump" + playerNum))
+        {
+            if (isOnGround)
+            {
+                isOnGround = false;
+                Vector2 jumpForce = new Vector2(0, jumpScale * 10);
+                rb2.AddForce(jumpForce);
+            }
+        }
 
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (CollisionIsWithGround(collision))
+        {
+            isOnGround = true;
+            print(isOnGround);
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(!CollisionIsWithGround(collision))
+        {
+            isOnGround = false;
+            print(isOnGround);
+        }
+    }
+
+    private bool CollisionIsWithGround(Collision2D collision)
+    {
+        bool is_with_ground = false;
+        foreach (ContactPoint2D c in collision.contacts)
+        {
+            Vector2 colDirVector = c.point + rb2.position;
+                                     if(colDirVector .y <0)
+            {
+                is_with_ground = true;
+            }
+        }
+
+        return is_with_ground;
+    }
+
+
+    public bool IsGrounded()
+    {
+        /*Todo Fix Raycasting solution
+                        Vector2 position = transform.position;
+                Vector2 direction = Vector2.down;
+                float distance = 1.0f;
+
+                Debug.DrawRay(position, direction, Color.green);
+                RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+                if (hit.collider != null)
+                {
+                    return true;
+                }
+
+                return false;*/
+
+
+
+
+        return true;
     }
 }
