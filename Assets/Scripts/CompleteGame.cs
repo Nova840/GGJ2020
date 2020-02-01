1f5df2d8c804f7cf8fec386606ba810e322fc83a
Adam Zmudzinski <adamzmudzinski2@gmail.com>
2020-02-01 17:23:17 -0500

    Add win screen when two players touch.

---
+++ Assets/Scripts/CompleteGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CompleteGame : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject winCanvas = null;
+
+    [SerializeField]
+    private float returnDelay = 5;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            winCanvas.SetActive(true);
+            StartCoroutine(ReturnToStart());
+        }
+    }
+
+    private IEnumerator ReturnToStart() {
+        yield return new WaitForSeconds(returnDelay);
+        SceneManager.LoadScene("Start");
+    }
+
+}
