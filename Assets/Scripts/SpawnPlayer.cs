eabef95fb2e646f37741869b2d49bcf30b69f964
Adam Zmudzinski <adamzmudzinski2@gmail.com>
2020-02-01 14:02:14 -0500

    Make players spawn in spawnpoints.

---
+++ Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour {
+
+    [SerializeField]
+    private bool spawnLeft = true;
+
+    private void Start() {
+        float zPos = transform.position.z;
+        Vector3 spawnPosition = spawnLeft ? LevelData.LeftSpawnpoint.position : LevelData.RightSpawnpoint.position;
+        spawnPosition.z = zPos;
+        transform.position = spawnPosition;
+    }
+
+}
