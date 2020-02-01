9e9f6f33f63b34575fdac98552085e2f23456a31
Adam Zmudzinski <adamzmudzinski2@gmail.com>
2020-02-01 11:17:46 -0500

    Add door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    private bool lifted = false;
+
+    private Vector3 originalPosition;
+
+    [SerializeField]
+    private float liftedOffset = 1;
+
+    [SerializeField]
+    private float liftSpeed = 5;
+
+    private void Awake() {
+        originalPosition = transform.position;
+    }
+
+    private void Update() {
+        Vector3 offset = lifted ? Vector3.up * liftedOffset : Vector3.zero;
+        transform.position = Vector3.MoveTowards(transform.position, originalPosition + offset, liftSpeed * Time.deltaTime);
+    }
+
+
+    public void SetLifted(bool lifted) {
+        this.lifted = lifted;
+    }
+
+}
