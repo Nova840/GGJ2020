2030682e2706d86e1103e4258a6e28faae68bc6b
Adam Zmudzinski <adamzmudzinski2@gmail.com>
2020-02-01 19:26:25 -0500

    fix bug where game could not finish when the two players touch

--- Assets/Scripts/CompleteGame.cs
+++ Assets/Scripts/CompleteGame.cs
@@ -11,8 +11,11 @@ public class CompleteGame : MonoBehaviour {
     [SerializeField]
     private float returnDelay = 5;
 
+    private static bool alreadyCompleted = false;
+
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Player")) {
+        if (!alreadyCompleted && collision.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            alreadyCompleted = true;
             winCanvas.SetActive(true);
             StartCoroutine(ReturnToStart());
         }
@@ -21,6 +24,10 @@ public class CompleteGame : MonoBehaviour {
     private IEnumerator ReturnToStart() {
         yield return new WaitForSeconds(returnDelay);
         SceneManager.LoadScene("Start");
+    }
+
+    private void OnDestroy() {
+        alreadyCompleted = false;
     }
 
 }
