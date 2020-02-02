049d5c583a431a792a1b24426573c335ce31e54b
Victor  Payano <vick.sensei@gmail.com>
2020-02-02 13:40:28 -0500

    Fixed Tutorial, added scaler

--- Assets/Scripts/SpawnPlayer.cs
+++ Assets/Scripts/SpawnPlayer.cs
@@ -7,11 +7,17 @@ public class SpawnPlayer : MonoBehaviour {
     [SerializeField]
     private bool spawnLeft = true;
 
+
     private void Start() {
+        float scaler;
         float zPos = transform.position.z;
         Vector3 spawnPosition = spawnLeft ? LevelData.LeftSpawnpoint.position : LevelData.RightSpawnpoint.position;
         spawnPosition.z = zPos;
         transform.position = spawnPosition;
+        scaler = LevelData.LeftSpawnpoint.gameObject.GetComponent<SpawnPoints>().plyrScale;
+        transform.localScale = new Vector3(scaler, scaler, 0);
+        transform.gameObject.GetComponent<PlayerController>().raycastDistance *= scaler;
+
     }
 
 }
