7d90513ea4aba04bf90d975584fe2c592b824635
Adam Zmudzinski <adamzmudzinski2@gmail.com>
2020-02-06 16:58:33 -0500

    Add cursor lock.

---
+++ Assets/Scripts/LockCursor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+public class LockCursor : MonoBehaviour {
+
+    private void Start() {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnDestroy() {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+}
