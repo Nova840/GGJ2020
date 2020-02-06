8678086dedf343b439c3449b2a7bcf6ded9e71b4
Adam Zmudzinski <adamzmudzinski2@gmail.com>
2020-02-06 16:56:13 -0500

    Made escape button go back to previous scene.

---
+++ Assets/Scripts/LoadSceneOnCancel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+public class LoadSceneOnCancel : MonoBehaviour {
+
+    [SerializeField]
+    private string sceneName = "";
+
+    private void Update() {
+        if (Input.GetButtonDown("Cancel")) {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+}
---
+++ Assets/Scripts/QuitGameOnCancel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+public class QuitGameOnCancel : MonoBehaviour {
+
+    private void Update() {
+        if (Input.GetButtonDown("Cancel")) {
+            Application.Quit();
+#if UNITY_EDITOR
+            Debug.Log("Can't quit game from editor.");
+#endif
+        }
+    }
+
+}
