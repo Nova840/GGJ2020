14a3cf95ef383eb7fce6566461691e65c92023f2
Adam Zmudzinski <adamzmudzinski2@gmail.com>
2020-02-09 00:51:27 -0500

    Fix bugs with fireworks not loading sometimes depending on which collider hit.

--- Assets/Scripts/CompleteGame.cs
+++ Assets/Scripts/CompleteGame.cs
@@ -16,63 +16,62 @@ public class CompleteGame : MonoBehaviour {
 
     [SerializeField]
     private float returnDelay = 5;
-    
+
     public static bool AlreadyCompleted { get; private set; } = false;
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (!AlreadyCompleted && collision.gameObject.layer == LayerMask.NameToLayer("Player")) {
             AlreadyCompleted = true;
             winCanvas.SetActive(true);
-            transform.parent.GetComponent<Animator>().SetInteger("Player", transform.parent.GetComponent<PlayerController>().PlayerNum);
-            transform.parent.GetComponent<Animator>().SetBool("Won", true);
-            collision.collider.transform.parent.GetComponent<Animator>().SetInteger("Player", collision.collider.transform.parent.GetComponent<PlayerController>().PlayerNum);
-            collision.collider.transform.parent.GetComponent<Animator>().SetBool("Won", true);
 
-            print(fireworkPrefab);
-            if (fireworkPrefab != null)
-            {
-                StartCoroutine(PlayFireworks(transform.parent, collision.collider.transform.parent));
+            Animator animator = GetComponentInParent<Animator>();
+            PlayerController playerController = GetComponentInParent<PlayerController>();
+
+            Animator otherAnimator = collision.transform.GetComponentInParent<Animator>();
+            PlayerController otherPlayerController = collision.transform.GetComponentInParent<PlayerController>();
+
+            animator.SetInteger("Player", playerController.PlayerNum);
+            animator.SetBool("Won", true);
+
+            otherAnimator.SetInteger("Player", otherPlayerController.PlayerNum);
+            otherAnimator.SetBool("Won", true);
+
+            if (fireworkPrefab != null) {
+                StartCoroutine(PlayFireworks(transform.root, collision.transform.root));
             }
             StartCoroutine(ReturnToStart());
         }
     }
 
     private IEnumerator ReturnToStart() {
-
         yield return new WaitForSeconds(returnDelay);
         SceneManager.LoadScene("Start");
     }
 
-    private IEnumerator PlayFireworks(Transform One, Transform Two)
-    {
+    private IEnumerator PlayFireworks(Transform One, Transform Two) {
         Transform active;
         int fireWorksCount = Random.Range(minFireworks, maxFireworks);
         int PlayerOrigin = Random.Range(1, 2);
-        for (int i = 0; i < fireWorksCount; i++)
-        {
-            float x, y,r, g, b;
+        for (int i = 0; i < fireWorksCount; i++) {
+            float x, y, r, g, b;
 
             x = Random.Range(-2f, 2f);
             y = Random.Range(-2f, 2f);
-            if (PlayerOrigin %2 == 1)
-            {
+            if (PlayerOrigin % 2 == 1) {
                 active = One;
-            }
-            else
-            {
+            } else {
                 active = Two;
             }
             GameObject fw = Instantiate(fireworkPrefab, new Vector3(active.position.x + x, active.position.y + y, active.position.z), Quaternion.identity, active);
 
-            r = Random.Range(100/255, 1f);
-            g = Random.Range(100/255, 1f);
-            b = Random.Range(100/255, 1f);
+            r = Random.Range(100f / 255f, 1f);
+            g = Random.Range(100f / 255f, 1f);
+            b = Random.Range(100f / 255f, 1f);
             fw.GetComponentInChildren<SpriteRenderer>().color = new Color(r, g, b);
 
             PlayerOrigin++;
             yield return new WaitForSeconds(returnDelay / fireWorksCount);
         }
-
     }
 
     private void OnDestroy() {
