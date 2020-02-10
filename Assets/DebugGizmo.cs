db96bf7e4b5b79a3ebaf58448cbd167818eeeb9d
vicksensei <vick.sensei@gmail.com>
2020-02-10 18:29:43 -0500

    added Test Level

---
+++ Assets/DebugGizmo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugGizmo : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, new Vector3 (1,1,1));
+    }
+}
---
+++ Assets/ProgressGizmo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressGizmo : MonoBehaviour
+{
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+}
